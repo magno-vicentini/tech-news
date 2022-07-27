63c91360f02d74281725d306a09a12858b6d0343
Magno Vicentini Serra <magno-vicentini@hotmail.com>
2022-07-27 16:03:07 -0300

    req 10

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,6 +1,15 @@
+from tech_news.database import get_collection
 # Requisito 10
+
+
 def top_5_news():
-    """Seu código deve vir aqui"""
+    all_news = get_collection()
+    news_list = list(all_news.find({}).sort([
+        ('comments_count', -1),
+        ('title', 1)
+    ]).limit(5))
+
+    return [(news['title'], news['url']) for news in news_list]
 
 
 # Requisito 11
